b2e44062104c8b46a6b414fb038339ceec5b68bf
toystory23 <ghlduq@naver.com>
2023-08-31 22:58:24 +0900

    DB페이지 구성 컴포넌트 업데이트

--- src/interface/DataInterface.tsx
+++ src/interface/DataInterface.tsx
@@ -5,3 +5,20 @@ export interface SettlementData {
   quantity: number;
   count: number;
 }
+export interface Ingredient {
+  name: string;
+  quantity: number;
+  units: string;
+}
+
+export interface FoodDto {
+  id: number;
+  name: string;
+  fixedPrice: number;
+  primePrice: number;
+  ingredients: Ingredient[];
+}
+
+export interface Data {
+  foodDtoList: FoodDto[];
+}
---
+++ src/recoil/DBAtom.ts
@@ -0,0 +1,11 @@
+import {atom} from 'recoil';
+
+export const DBAtom = atom({
+    key:'DBAtomArr',
+    default:[]
+})
+
+export const Ingredients = atom({
+    key:'Ingredients',
+    default:[]
+})
