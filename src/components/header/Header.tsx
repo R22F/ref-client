0a4375721e5249ca89f7fcdf860e28739aa29855
jeontigger <boy9609@naver.com>
2023-09-02 14:37:32 +0900

    #25 로그인 버튼 css 구현 다른 네비 요소도 해상도에도 글자 한 줄로 유지되도록 함

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { TotalGNB } from "../GNB/TotalGNB";
-import Button from "@mui/material/Button";
-
 export const Header = () => {
   const [isLogin, setIsLogin] = useState(false);
   const handleLogin = () => {
     setIsLogin(!isLogin);
   };
   useEffect(() => {}, [isLogin]);
+  const logInButtonColor = (isLogin: boolean) => {
+    return isLogin === true
+      ? "bg-red-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded whitespace-nowrap"
+      : "bg-gray-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded hover:shadow-lg whitespace-nowrap";
+  };
 
   return (
     <>
@@ -15,9 +18,9 @@ export const Header = () => {
         <TotalGNB />
         <div className="flex items-center mr-4">
           <div className=" pr-4">{isLogin ? "OOO님 안녕하세요!" : ""}</div>
-          <Button variant="contained" onClick={handleLogin}>
+          <button onClick={handleLogin} className={logInButtonColor(isLogin)}>
             {isLogin ? "LOG OUT" : "LOG IN"}
-          </Button>
+          </button>
         </div>
       </div>
       {/* <Button variant="contained" style={{"background":"red"}}>test button</Button> */}
