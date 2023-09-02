ed32495d37a4379def38a86e9526be56bf48c5c4
jeontigger <boy9609@naver.com>
2023-09-02 16:26:14 +0900

    #25 정산페이지 메뉴판 css 수정 div와 span이던거 table로 수정

--- src/components/databox/SettleDatabox.tsx
+++ src/components/databox/SettleDatabox.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { SettlementData } from '../../interface/DataInterface';
-import data from '../../pages/Settlement/data.json';
+import { useEffect, useState } from "react";
+import { SettlementData } from "../../interface/DataInterface";
+import data from "../../pages/Settlement/data.json";
 
 export const SettleDatabox = () => {
   const [itemCounts, setItemCounts] = useState<Record<string, number>>({});
@@ -12,34 +12,70 @@ export const SettleDatabox = () => {
     });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     let sum = 0;
     for (let itemId in itemCounts) {
-      sum += (itemCounts[itemId]||0) * (data.data.find((item) => item.id === parseInt(itemId))?.price||0);
+      sum +=
+        (itemCounts[itemId] || 0) *
+        (data.data.find((item) => item.id === parseInt(itemId))?.price || 0);
     }
     setTotalPrice(sum);
   }, [itemCounts]);
 
   return (
-    <div className='flex justify-center flex-col w-[60rem]'>
-      <div className='flex justify-end font-bold mr-1 border-b-2 border-black'>합계 : {totalPrice.toLocaleString()}</div>
-      {data.data.map((item: SettlementData): any => {
-        return (
-          <div key={item.id} className='my-1 py-1 flex justify-between '>
-            <span className='w-[15rem]'>음식 명 : {item.dish}</span>
-            <span className='w-[10rem]'>단가 : {item.price.toLocaleString()}</span>
-            <input
-              type='number'
-              min='0'
-              max='9999'
-              placeholder='판매 갯수'
-              className=' border-b-2 border-black w-24'
-              onChange={(e) => handleCountChange(item.id, e.target.value)}
-            ></input>
-            <span className='w-[6rem]'>소계 : {((itemCounts[item.id] ?? 0) * item.price).toLocaleString()}</span>
-          </div>
-        );
-      })}
+    <div className="flex justify-center flex-col w-[60rem] overflow-x-auto sm:-mx-6 lg:-mx-8 border-4 rounded-md px-4 py-4">
+      <div className="flex justify-end font-bold mr-1 border-b-2 border-black pb-2 text-red-400">
+        합계 : {totalPrice.toLocaleString()} 원
+      </div>
+      <table className="min-w-full text-center text-sm font-light">
+        <thead className="border-b bg-neutral-50 font-medium dark:border-neutral-500 dark:text-neutral-800">
+          <tr>
+            <th scope="col" className="w-4 px-6 py-4"></th>
+            <th scope="col" className=" px-6 py-4 text-right">
+              요리명
+            </th>
+            <th scope="col" className=" px-6 py-4 text-right">
+              단가
+            </th>
+            <th scope="col" className=" px-6 py-4 text-right">
+              판매개수
+            </th>
+            <th scope="col" className=" px-6 py-4 text-right mr-4">
+              소계
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.data.map((item: SettlementData, idx: number): any => {
+            return (
+              <tr className="border-b dark:border-neutral-500">
+                <td>{idx + 1}</td>
+                <td className="whitespace-nowrap  px-6 py-4 font-medium text-right">
+                  {item.dish}
+                </td>
+                <td className="whitespace-nowrap  px-6 py-4 text-right">
+                  {item.price.toLocaleString()} 원
+                </td>
+                <td className="whitespace-nowrap  px-6 py-4 text-right">
+                  <input
+                    type="number"
+                    min="0"
+                    max="9999"
+                    placeholder="0"
+                    className=" border-b-2 border-black w-24 text-right w-12"
+                    onChange={(e) => handleCountChange(item.id, e.target.value)}
+                  ></input>
+                  <> 개</>
+                </td>
+                <td className="whitespace-nowrap  px-6 py-4 text-right">
+                  {((itemCounts[item.id] ?? 0) * item.price).toLocaleString()}{" "}
+                  원
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/pages/Settlement/index.tsx
+++ src/pages/Settlement/index.tsx
@@ -1,7 +1,6 @@
-import Button from '@mui/material/Button';
-import { SettleDatabox } from '../../components/databox/SettleDatabox';
-import { DayCount } from '../../components/databox/DayCount';
-import { useState } from 'react';
+import { SettleDatabox } from "../../components/databox/SettleDatabox";
+import { DayCount } from "../../components/databox/DayCount";
+import { useState } from "react";
 
 export const Settlement = () => {
   const [selectedDate, setSelectedDate] = useState(DayCount());
@@ -11,10 +10,17 @@ export const Settlement = () => {
   };
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col justify-evenly mt-28">
-        <div className="flex absolute w-52 justify-between top-[8rem] right-[11rem]">
-          <input type="date" value={selectedDate} onChange={handleDateChange}></input>
-          <Button variant="contained">정산</Button>
+      <div className="flex flex-col justify-between mt-28 items-center">
+        <div className="flex justify-between items-center ml-auto mb-4">
+          <input
+            type="date"
+            value={selectedDate}
+            onChange={handleDateChange}
+            className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-red-400 flex-grow"
+          />
+          <button className="bg-white hover:bg-red-400 hover:border-red-100 hover:text-red-100 text-red-400 font-semibold py-2 px-4 border border-red-400 rounded shadow ml-4">
+            정산
+          </button>
         </div>
         <SettleDatabox />
       </div>
