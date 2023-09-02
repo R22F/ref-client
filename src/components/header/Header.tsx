d828a08f2f9162869abbc68fc5e795be96cbfe4b
jeontigger <boy9609@naver.com>
2023-09-02 14:07:38 +0900

    #25 location.pathname을 이용한 네비게이션 css 구현

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import { TotalGNB } from '../GNB/TotalGNB';
-import Button from '@mui/material/Button';
+import { useEffect, useState } from "react";
+import { TotalGNB } from "../GNB/TotalGNB";
+import Button from "@mui/material/Button";
 
 export const Header = () => {
-  const [isLogin,setIsLogin]=useState(false);
+  const [isLogin, setIsLogin] = useState(false);
   const handleLogin = () => {
     setIsLogin(!isLogin);
-  }
-  useEffect(()=>{
-
-  },[isLogin])
+  };
+  useEffect(() => {}, [isLogin]);
 
   return (
     <>
-      <div className="flex justify-between mt-8">
+      <div className="flex justify-between">
         <TotalGNB />
-        <div className='flex mr-4'>
-          <div className=' pr-4 content-center'>{isLogin?'OOO님 안녕하세요!':''}</div>
-          <Button variant="contained" onClick={handleLogin}>{isLogin?'LOG OUT':'LOG IN'}</Button>
+        <div className="flex items-center mr-4">
+          <div className=" pr-4">{isLogin ? "OOO님 안녕하세요!" : ""}</div>
+          <Button variant="contained" onClick={handleLogin}>
+            {isLogin ? "LOG OUT" : "LOG IN"}
+          </Button>
         </div>
       </div>
       {/* <Button variant="contained" style={{"background":"red"}}>test button</Button> */}
