dc81c0556783189dac566f39d95c669639380f8e
toystory23 <ghlduq@naver.com>
2023-10-03 15:00:21 +0900

    commit for pull

--- src/recoil/DBAtom.ts
+++ src/recoil/DBAtom.ts
@@ -1,29 +1,28 @@
-import { atom } from "recoil";
-import { FoodData, FoodDto, IngredientDto } from "../interface/DataInterface";
+import { atom } from 'recoil';
+import { FoodDto, IngredientDto } from '../interface/DataInterface';
 
 export const DBAtom = atom<FoodDto[]>({
-  key: "DBAtomArr",
+  key: 'DBAtomArr',
   default: [],
 });
 
 export const Ingredients = atom({
-  key: "Ingredients",
+  key: 'Ingredients',
   default: [],
 });
 
 export const EditMode = atom({
-  key: "edit",
+  key: 'edit',
   default: false,
 });
 export const foodData = atom({
-  key: "foodData",
+  key: 'foodData',
   default: {
     quantity: 0,
-    name: "",
+    name: '',
   },
 });
-
 export const InventoryDto = atom<IngredientDto[]>({
-  key: "IvnAtomArr",
+  key: 'IvnAtomArr',
   default: [],
 });
