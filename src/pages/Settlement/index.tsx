388936e857b222a1b3df3c5def91f33a92bdf560
jeontigger <boy9609@naver.com>
2023-10-15 22:41:59 +0900

    #41 로그인 확인 후 false => navigate로 메인

--- src/pages/Settlement/index.tsx
+++ src/pages/Settlement/index.tsx
@@ -1,30 +1,45 @@
 import { SettleDatabox } from "../../components/databox/SettleDatabox";
 import { DayCount } from "../../components/databox/DayCount";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { Login } from "../../recoil/DBAtom";
 
 export const Settlement = () => {
   const [selectedDate, setSelectedDate] = useState(DayCount());
+  const isLogin = useRecoilValue(Login);
+  const navigate = useNavigate();
 
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedDate(event.target.value);
   };
 
+  useEffect(() => {
+    if (!isLogin) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col justify-between mt-28 items-center">
-        <div className="flex justify-between items-center ml-auto mb-4">
-          <input
-            type="date"
-            value={selectedDate}
-            onChange={handleDateChange}
-            className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline flex-grow"
-          />
-          <button className="bg-white hover:bg-red-400 hover:border-red-100 hover:text-red-100 text-red-400 font-semibold py-2 px-4 border border-red-400 rounded shadow ml-4">
-            정산
-          </button>
+      {isLogin ? (
+        <div className="flex flex-col justify-between mt-28 items-center">
+          <div className="flex justify-between items-center ml-auto mb-4">
+            <input
+              type="date"
+              value={selectedDate}
+              onChange={handleDateChange}
+              className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline flex-grow"
+            />
+            <button className="bg-white hover:bg-red-400 hover:border-red-100 hover:text-red-100 text-red-400 font-semibold py-2 px-4 border border-red-400 rounded shadow ml-4">
+              정산
+            </button>
+          </div>
+          <SettleDatabox />
         </div>
-        <SettleDatabox />
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
