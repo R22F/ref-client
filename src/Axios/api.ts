2ef100fb0920d9909ae989ac0339e7aff3cebaf2
Sungwon Choi <csw2757@naver.com>
2024-01-06 23:12:58 +0900

    feat: status가 존재하지 않을 경우 대응

--- src/Axios/api.ts
+++ src/Axios/api.ts
@@ -21,19 +21,24 @@ export const useAxiosInstance = (): AxiosInstance => {
 };
 
 export const checkTokenValidate = (res: any, setIsModalOpen:any):any=> {
-  console.log(res)
-  switch (res.response.status) {
-    case 417:
-      window.alert("로그인 정보가 만료되었습니다.")
-      localStorage.removeItem("Authorization")
-      setIsModalOpen(true)
-      break;
-    case 401: case 403:
-      window.alert("로그인이 필요합니다.")
-      window.location.replace("/")
-      break;
-    default:
-      break;
+  try {
+    const code = res.response.status
+    switch (code) {
+      case 417:
+        window.alert("로그인 정보가 만료되었습니다.")
+        localStorage.removeItem("Authorization")
+        setIsModalOpen(true)
+        break;
+      case 401:
+      case 403:
+        window.alert("로그인이 필요합니다.")
+        window.location.replace("/")
+        break;
+      default:
+        break;
+    }
+  }catch (e) {
+    console.log("Network Error, "+e)
   }
   return res
 }
